2df2f52fb9aa0bd59c74656dd90d422b83ea4fc2
Bala Venkat <balavenkat@hotmail.com>
2018-11-13 22:13:32 -0800

    Add files via upload
    four cheese+pepperoni Rank:1 No of Orders:203
    mozzarella cheese+pepperoni Rank:2 No of Orders:155
    beef+pepperoni Rank:3 No of Orders:122
    bacon+pepperoni Rank:4 No of Orders:121
    pepperoni+sausage Rank:5 No of Orders:96
    italian sausage+pepperoni Rank:6 No of Orders:85
    jalapenos+pepperoni Rank:7 No of Orders:67
    mushrooms+pepperoni Rank:8 No of Orders:60
    bacon+pineapple Rank:9 No of Orders:39
    bacon+canadian bacon+pineapple+sausage Rank:10 No of Orders:30
    bacon+mushrooms Rank:11 No of Orders:30
    black olives+pepperoni Rank:12 No of Orders:28
    bacon+four cheese Rank:13 No of Orders:25
    bacon+beef Rank:14 No of Orders:24
    beef+four cheese Rank:15 No of Orders:24
    four cheese+mozzarella cheese Rank:16 No of Orders:23
    bacon+italian sausage Rank:17 No of Orders:21
    ham+pineapple Rank:18 No of Orders:21
    ham+pepperoni Rank:19 No of Orders:20
    pepperoni+pineapple Rank:20 No of Orders:19

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace VenkatBalaOLO
+{
+    class Program
+    {
+        private static List<oloorders> DownloadAndDeserializeJsonData(string url) 
+        {
+            using (var webClient = new WebClient())
+            {
+                var jsonData = string.Empty;
+                try
+                {
+                    jsonData = webClient.DownloadString(url);
+                }
+                catch (Exception) { }
+                
+                return (List<oloorders>)JsonConvert.DeserializeObject<List<oloorders>>(jsonData);
+
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+            List<oloorders> oloorderslist = DownloadAndDeserializeJsonData("http://files.olo.com/pizzas.json");
+                        
+            var orderhavingmorethanonetoppings = from d in oloorderslist where d.toppings.Count() > 1 select d;
+            List<string> SortedconcatinatedToppingsList = new List<string>();
+            foreach (oloorders oloorder in orderhavingmorethanonetoppings)
+            {
+                SortedconcatinatedToppingsList.Add(string.Join("+", (oloorder.toppings.OrderBy(t => t.ToString()).ToArray())));               
+                
+            }
+
+            var top20List = (from r in SortedconcatinatedToppingsList
+                      orderby r.ToString()
+                     group r by r.ToString() into grp
+                     select new { key = grp.Key, cnt = grp.Count() }).OrderByDescending(c => c.cnt).Take(20);
+
+            //print the top 20 topping combinations
+            foreach(var orders in top20List)
+            {
+                Console.Write(orders.key + " " + orders.cnt + Environment.NewLine);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        public class oloorders
+        {
+            public List<string> toppings { get; set; }
+        }
+    }
+}
